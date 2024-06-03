527bee99115f0dfb995f2e50e2dc0546e130b9ba
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2024-06-03 18:55:39 -0400

    Implement WIP Oura driver with status, configure, and daily metrics

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Oura driver
+
+WIP
--- index.ts
+++ index.ts
@@ -5,11 +5,14 @@
  */
 
 import { root, state, resolvers } from "membrane";
-import { createHandler } from "./utils";
 
 export interface State {
   personalAccessToken?: string;
 }
+
+/**
+ * HELPERS
+ */
 
 const OURA_V2_API_URL = "https://api.ouraring.com/v2/usercollection";
 
@@ -20,54 +23,162 @@ const api = async <T>(path: string): Promise<T> => {
   });
 
   if (response.status !== 200) {
-    throw new Error(`HTTP ${response.status}: ${response.text()}`);
+    throw new Error(`HTTP ${response.status}: ${await response.text()}`);
   }
 
-  const { data } = await response.json();
-
-  return data;
+  return response.json();
 };
 
-export const status: resolvers.Root["status"] = () => {
-  return !state.personalAccessToken
-    ? "[Add Personal Access Token](:configure)"
-    : "Ready";
-};
-
-// To set up a personal access token: https://cloud.ouraring.com/personal-access-tokens/new
-export const configure: resolvers.Root["configure"] = async ({
-  personalAccessToken,
+// TODO: clean up and/or extract helper(s)
+// Nits - FIXME:
+// 1. Pagination sorts in chronological order on each page
+// 2. Pagination includes bordering items on prev/next page
+const paginate = (args: {
+  startDate?: string;
+  endDate?: string;
+  page?: number;
+  pageSize?: number;
 }) => {
-  state.personalAccessToken = personalAccessToken;
-  root.statusChanged.$emit();
+  const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
+  let { startDate, endDate, page, pageSize } = args;
+  let nextArgs;
+
+  page = Math.max(page ?? 1, 1);
+  pageSize = Math.min(10, pageSize ?? 7); // TODO: bound by actual Oura API limit?
+
+  const end = endDate ? new Date(endDate) : new Date();
+  const start = startDate ? new Date(startDate) : new Date();
+
+  if (end.getTime() - start.getTime() > pageSize * ONE_DAY_IN_MS) {
+    end.setTime(end.getTime() - (page - 1) * pageSize * ONE_DAY_IN_MS);
+    start.setTime(end.getTime() - pageSize * ONE_DAY_IN_MS);
+
+    nextArgs = {
+      startDate,
+      endDate,
+      page: page + 1,
+      pageSize,
+    };
+  } else {
+    if (!startDate) {
+      start.setTime(end.getTime() - (pageSize - 1) * ONE_DAY_IN_MS);
+    }
+    nextArgs = null;
+  }
+
+  const searchParams = new URLSearchParams();
+  searchParams.append("start_date", start.toISOString().split("T")[0]);
+  searchParams.append("end_date", end.toISOString().split("T")[0]);
+
+  return {
+    params: searchParams.toString(),
+    nextArgs,
+  };
 };
 
-export const daily: resolvers.Daily = {
-  activity: createHandler("daily_activity", api),
-  sleep: createHandler("daily_sleep", api),
-  spo2: createHandler("daily_spo2", api),
-  readiness: createHandler("daily_readiness", api),
-  stress: createHandler("daily_stress", api),
+/**
+ * FIELDS, ACTIONS
+ */
+
+export const Root: resolvers.Root = {
+  status() {
+    return !state.personalAccessToken
+      ? "[Add Personal Access Token](:configure)"
+      : "Ready";
+  },
+
+  // To set up a personal access token: https://cloud.ouraring.com/personal-access-tokens/new
+  async configure({ personalAccessToken }) {
+    state.personalAccessToken = personalAccessToken;
+    root.statusChanged.$emit();
+  },
+
+  dailySleep: () => ({}),
+  dailyActivity: () => ({}),
+  dailyReadiness: () => ({}),
+  dailyStress: () => ({}),
 };
 
-// TODO: Implement the rest of the endpoints
+export const DailySleep: resolvers.DailySleep = {
+  gref: (_, { obj }) => root.dailySleep.one({ id: obj.id }),
+};
 
-// export const heartrate();
+export const DailySleepCollection: resolvers.DailySleepCollection = {
+  one: ({ id }) => api(`/daily_sleep/${id}`),
 
-// export const personalInfo();
+  page: async (args, { self }) => {
+    const { params, nextArgs } = paginate(args);
 
-// export const tag();
+    const { data } = await api<{ data: resolvers.DailySleep }>(
+      `/daily_sleep?${params}`
+    );
 
-// export const enhancedTag();
+    return {
+      items: data,
+      next: nextArgs ? self.page(nextArgs) : null,
+    };
+  },
+};
 
-// export const workout();
+export const DailyActivity: resolvers.DailyActivity = {
+  gref: (_, { obj }) => root.dailyActivity.one({ id: obj.id }),
+};
 
-// export const session();
+export const DailyActivityCollection: resolvers.DailyActivityCollection = {
+  one: ({ id }) => api(`/daily_activity/${id}`),
 
-// export const sleep();
+  page: async (args, { self }) => {
+    const { params, nextArgs } = paginate(args);
 
-// export const sleepTime();
+    const { data } = await api<{ data: resolvers.DailyActivity }>(
+      `/daily_activity?${params}`
+    );
 
-// export const restModePeriod();
+    return {
+      items: data,
+      next: nextArgs ? self.page(nextArgs) : null,
+    };
+  },
+};
 
-// export const ringConfiguration();
+export const DailyReadiness: resolvers.DailyReadiness = {
+  gref: (_, { obj }) => root.dailyReadiness.one({ id: obj.id }),
+};
+
+export const DailyReadinessCollection: resolvers.DailyReadinessCollection = {
+  one: ({ id }) => api(`/daily_readiness/${id}`),
+
+  page: async (args, { self }) => {
+    const { params, nextArgs } = paginate(args);
+
+    const { data } = await api<{ data: resolvers.DailyReadiness }>(
+      `/daily_readiness?${params}`
+    );
+
+    return {
+      items: data,
+      next: nextArgs ? self.page(nextArgs) : null,
+    };
+  },
+};
+
+export const DailyStress: resolvers.DailyStress = {
+  gref: (_, { obj }) => root.dailyStress.one({ id: obj.id }),
+};
+
+export const DailyStressCollection: resolvers.DailyStressCollection = {
+  one: ({ id }) => api(`/daily_stress/${id}`),
+
+  page: async (args, { self }) => {
+    const { params, nextArgs } = paginate(args);
+
+    const { data } = await api<{ data: resolvers.DailyStress }>(
+      `/daily_stress?${params}`
+    );
+
+    return {
+      items: data,
+      next: nextArgs ? self.page(nextArgs) : null,
+    };
+  },
+};
